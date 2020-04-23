c9e2121444b5ae1456e119d4e4453429a8926e5d
yuchen <smirkiiiv@gmail.com>
2020-04-23 09:15:12 +0800

    whoCanHelp Demo

--- TaiwanCanHelp/ViewController.swift
+++ TaiwanCanHelp/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
+
+let renderImages  = ["bear.png","Bluebird.png","cat.png","fish.png"]
+let showLabel = ["Taiwan Number 1","HomeLand","Formosa","Well You Know"]
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeText: UILabel!
+    
+    @IBOutlet weak var changedSegment: UISegmentedControl!
+    
+    @IBOutlet weak var changedImage: UIImageView!
+    
+    var num = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+      changedImage.image=UIImage(named: renderImages[num])
+      changeText.text = showLabel[num]
+      changedSegment.selectedSegmentIndex = num
 
+    }
+    
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+        num = Int(sender.selectedSegmentIndex)
+        changedImage.image=UIImage(named: renderImages[num])
+        changeText.text = showLabel[num]
+        
+    }
+    
+    
+    @IBAction func changeButton(_ sender: UIButton) {
+        num = Int.random(in: 0...renderImages.count-1)
+        changedSegment.selectedSegmentIndex = num
+        changedImage.image=UIImage(named: renderImages[num])
+        changeText.text = showLabel[num]
+        
+    }
+    
 
 }
 
